f4602792f8333adfe6cb3065e68a62c2f497d9e2
Nina <nina05022002@mail.ru>
2022-09-28 19:04:11 +0300

    создает двусвязный список и функции добавления нового элемента списка и вывода списка на экран

--- ConsoleApplication60/ConsoleApplication60.cpp
+++ ConsoleApplication60/ConsoleApplication60.cpp
@@ -1,20 +1,106 @@
-﻿// ConsoleApplication60.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+using namespace std;
+
+class List
+{
+public:
+	List();
+	~List();
+
+	void Show();
+	void push_back(int data);
+	int GetSize() { return Size; };
+	int& operator[](const int index);
+private:
+	class Node
+	{
+	public:
+		Node *pNext, *pPrev;
+		int data;
+		Node(int data = int(), Node *pPrev = nullptr, Node *pNext = nullptr)
+		{
+			this->data = data;
+			this->pPrev = pPrev;
+			this->pNext = pNext;
+		}
+	};
+	int Size;
+	Node *head, *tail;
+};
+
+List::List()
+{
+	Size = 0;
+	head = nullptr;
+	tail = nullptr;
+}
+
+List::~List()
+{
+}
+
+void List::Show()
+{
+	Node *temp = head;
+
+	while (temp != nullptr)
+	{
+		cout << temp->data << " ";
+		temp = temp->pNext;
+	}
+	cout << endl;
+}
+
+void List::push_back(int data)
+{
+	if (head == nullptr)
+	{
+		head = tail = new Node(data);
+	}
+	else
+	{
+		Node *temp = new Node(data, tail);
+		tail->pNext = temp;
+		tail = temp;
+	}
+
+	Size++;
+}
+
+int & List::operator[](const int index)
+{
+	int counter = 0;
+	Node *current = this->head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+		{
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "ru");
+	List lst;
+	int numbersCount;
+	cin >> numbersCount;
+
+	for (int i = 0; i < numbersCount; i++)
+	{
+		lst.push_back(rand() % 10);
+	}
+
+	for (int i = 0; i < lst.GetSize(); i++)
+	{
+		cout << lst[i] << endl;
+	}
+	lst.Show();
+	int a;
+	cin >> a;
+	cout << lst[a];
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
